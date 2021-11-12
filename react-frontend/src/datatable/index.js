f4875912eb1c8f3c13629101d3410768a9a83f05
KhyatiNaks <khyati.s@naksconsulting.com>
2021-11-12 18:31:15 +0530

    AD-32 UI includes cards of sites

--- react-frontend/src/datatable/index.js
+++ react-frontend/src/datatable/index.js
@@ -1,15 +1,75 @@
-import React from "react";
-import "../fetch_from_api.css";
+import React, { useState } from "react";
+import "../card.css";
 
 export default function Datatable({ data }) {
+  // const data_object = [
+  //   {site_code: data.site_code},
+  //   {location: data.location},
+  //   {media_type: data.media_type},
+  //   {price_per_month: data.display_cost},
+  // ];
+
+  const [priceDetails, setPriceDetails] = useState([]);
+  const [checkTrue, setCheckTrue] = useState(false);
+
+  function ifChecked(checkedId) {
+    // alert(checkedId);
+    console.log(checkedId);
+    priceDetails.push(checkedId);
+    setPriceDetails(priceDetails);
+    setCheckTrue(true);
+  }
+
   return (
     <div>
       <div id="empty_response"></div>
-      <table cellPadding={2} cellSpacing={2}>
+      <div className="parent_cards_total">
+        <div id="all_cards">
+          {data.map((rows) => (
+            <div className="check_card">
+              <input
+                type="checkbox"
+                id={rows.display_cost}
+                onChange={(e) => {
+                  ifChecked(e.target.id);
+                }}
+              />
+              <div className="media_card">
+                <img src="/image.jpg" />
+                <div>
+                  <div className="card_details">
+                    Site Code: {rows.site_code}
+                  </div>
+                  <div className="card_details">Location: {rows.location}</div>
+                  <div className="card_details">
+                    Media Type: {rows.media_type}
+                  </div>
+                </div>
+                <div className="card_details">
+                  Price per month: {rows.display_cost}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="total_price">{checkTrue?<div>
+            All Sites:<div>{priceDetails.map((elmnt) => (<p>{elmnt}</p>))}</div>
+              {/* {priceDetails.map((elmnt) => (
+                <p>{elmnt},</p>
+              ))} */}
+            {/* <i>Selected sites here with their respective prices</i> */}
+            <br />
+            Estimate Price: <div>{priceDetails}</div>
+            {/* <i>Total price of all sites</i> */}
+          </div>:""}
+          
+        </div>
+      </div>
+      {/* <table cellPadding={2} cellSpacing={2}>
         <thead>
-          {/* <tr>
+          <tr>
             {data[0] && Object.keys(data[0]).map((item) => <th>{item}</th>)}
-          </tr> */}
+          </tr>    ------this tr tag should be commented-------
           <th>Site Code</th>
           <th>Sub Environment</th>
           <th>State Name</th>
@@ -30,7 +90,7 @@ export default function Datatable({ data }) {
         <tbody>
           {data.map((rows) => (
             <tr>
-              {/* <td>{rows.uuid}</td> */}
+              <td>{rows.uuid}</td>      ------this td tag should be commented-------
               <td>{rows.site_code}</td>
               <td>{rows.sub_environment}</td>
               <td>{rows.state_name}</td>
@@ -47,13 +107,13 @@ export default function Datatable({ data }) {
               <td>{rows.display_cost}</td>
               <td>{rows.additional_size_comments}</td>
               <td>{rows.printing_material}</td>
-              {/* <td>{rows.onwer_of_media}</td>
-              <td>{rows.createdat}</td>
-              <td>{rows.updatedat}</td> */}
+              <td>{rows.onwer_of_media}</td>    ------this td tag should be commented-------
+              <td>{rows.createdat}</td>    ------this td tag should be commented-------
+              <td>{rows.updatedat}</td>    ------this td tag should be commented-------
             </tr>
           ))}
         </tbody>
-      </table>
+      </table> */}
       <div id="garbage"></div>
       {data.length === 0 && <p>No data found</p>}
     </div>
